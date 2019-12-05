a257d857f99036d0564b023b6b09b3af996d0c2f
jet_master101 <33845038+jetmaster101@users.noreply.github.com>
2019-12-05 10:37:02 -0700

    Update squarerootscalculator.py

--- squarerootscalculator.py
+++ squarerootscalculator.py
@@ -2,5 +2,3 @@ import math
 number = float(input("please evnter a number"))
 result = math.sqrt(number)
 print("the square root of ", number, " is: ",result)
-
-print("thanks for using the square root calculator!")
